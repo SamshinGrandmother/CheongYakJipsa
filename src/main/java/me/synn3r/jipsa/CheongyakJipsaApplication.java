c6b39aee5ee8c16077ce0613b2379fe6d7147931
wonderwall <gkszm63@hanssak.co.kr>
2025-04-08 13:36:48 +0900

    프로젝트 이름 변경

--- src/main/java/me/synn3r/jipsa/CheongyakJipsaApplication.java
+++ src/main/java/me/synn3r/jipsa/CheongyakJipsaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CheongYakJipsaApplication {
+public class CheongyakJipsaApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(CheongYakJipsaApplication.class, args);
+    SpringApplication.run(CheongyakJipsaApplication.class, args);
   }
 
 }
