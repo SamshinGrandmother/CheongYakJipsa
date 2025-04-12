9271e25e594c324cd149978bf87c430210ded3dc
gkszm <gkszm636@gmail.com>
2025-04-12 22:13:40 +0900

    로그인 화면 추가 및 레아아웃 추가

---
+++ src/main/java/me/synn3r/jipsa/core/mvc/base/LoginController.java
@@ -0,0 +1,14 @@
+package me.synn3r.jipsa.core.mvc.base;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+  @GetMapping(value={"/", "/login"})
+  public String loginPage(Model model) {
+    model.addAttribute("loginForm", new LoginForm());
+    return "pages/SignIn";
+  }
+}
---
+++ src/main/java/me/synn3r/jipsa/core/mvc/base/LoginForm.java
@@ -0,0 +1,11 @@
+package me.synn3r.jipsa.core.mvc.base;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginForm {
+  private String email;
+  private String password;
+}
