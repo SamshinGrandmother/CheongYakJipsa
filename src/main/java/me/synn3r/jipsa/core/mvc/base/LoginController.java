8dc30b1ee993caaea5a2fe2c389c5aa8bd8061ef
gkszm <gkszm636@gmail.com>
2025-04-20 00:29:46 +0900

    SpringSecurity 를 MVC로 전환

--- src/main/java/me/synn3r/jipsa/core/mvc/base/LoginController.java
+++ src/main/java/me/synn3r/jipsa/core/mvc/base/LoginController.java
@@ -3,12 +3,15 @@ package me.synn3r.jipsa.core.mvc.base;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+
   @GetMapping(value={"/", "/login"})
-  public String loginPage(Model model) {
+  public String loginPage(Model model, @RequestParam(value="reason", required = false) String reason) {
     model.addAttribute("loginForm", new LoginForm());
+    model.addAttribute("reason", reason);
     return "pages/SignIn";
   }
 }
