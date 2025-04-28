e38c156f6c42eaad6bb5778813880b91033b4a55
leunj8751 <leunj87511@gmail.com>
2025-04-29 00:40:09 +0900

    swagger session 인증추가

--- src/main/java/me/synn3r/jipsa/core/config/SwaggerConfig.java
+++ src/main/java/me/synn3r/jipsa/core/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package me.synn3r.jipsa.core.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +14,13 @@ public class SwaggerConfig {
   @Bean
   public OpenAPI openAPI() {
     return new OpenAPI()
-      .components(new Components())
+      .addSecurityItem(new SecurityRequirement().addList("SESSION"))
+      .components(new Components().addSecuritySchemes("SESSION",
+        new SecurityScheme()
+          .name("SESSION")
+          .type(SecurityScheme.Type.APIKEY)
+          .in(SecurityScheme.In.HEADER)
+          .name("Cookie")))
       .info(apiInfo());
   }
 
