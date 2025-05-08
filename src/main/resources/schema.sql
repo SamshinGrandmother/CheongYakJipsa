1fc873529fb2a600a9f1f8d025d9a60cbf59290f
leunj8751 <leunj87511@gmail.com>
2025-05-09 00:56:23 +0900

    member 테이블 스키마 변경  - userId 컬럼 추가, 전화번호 컬럼 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,17 @@
 create table if not exists member
 (
     id          bigint auto_increment primary key,
+    user_id     varchar(16)  not null,
     name        varchar(32)  not null,
     email       varchar(32)  not null,
     password    varchar(128) not null,
     role        varchar(8)   not null,
     delete_type varchar(6)   not null,
     updated_at  timestamp    not null,
-    created_at  timestamp    not null
+    created_at  timestamp    not null,
+    phone_number varchar(20) default null,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `member_unique` (`user_id`)
 );
 
 create table if not exists team
