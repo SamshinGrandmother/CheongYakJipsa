89734b4ab5703f73ab051bcfdca0baa67d08b076
gkszm <gkszm636@gmail.com>
2025-05-25 00:42:06 +0900

    주택 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,15 @@
 create table if not exists member
 (
-    id          bigint auto_increment comment '사용자 및 관리자의 식별자',
-    user_id     varchar(16)  not null comment '사용자 및 관리자가 인증 시 사용하는 ID',
-    name        varchar(32)  not null comment '사용자 및 관리자의 이름',
-    email       varchar(32)  not null comment '사용자 및 관리자의 이메일',
-    password    varchar(128) not null comment '사용자 및 관리자의 패스워드',
-    role        varchar(8)   not null comment '사용자 및 관리자의 권한(Enum:Role)',
+    id           bigint auto_increment comment '사용자 및 관리자의 식별자',
+    user_id      varchar(16)  not null comment '사용자 및 관리자가 인증 시 사용하는 ID',
+    name         varchar(32)  not null comment '사용자 및 관리자의 이름',
+    email        varchar(32)  not null comment '사용자 및 관리자의 이메일',
+    password     varchar(128) not null comment '사용자 및 관리자의 패스워드',
+    role         varchar(8)   not null comment '사용자 및 관리자의 권한(Enum:Role)',
     phone_number varchar(20) default null comment '사용자 및 관리자의 전화번호',
-    delete_type varchar(6)   not null comment '사용자 및 관리자의 삭제 여부(Enum:DeleteType)',
-    updated_at  timestamp    not null comment '사용자 및 관리자의 변경 시각',
-    created_at  timestamp    not null comment '사용자 및 관리자의 생성 시각',
+    delete_type  varchar(6)   not null comment '사용자 및 관리자의 삭제 여부(Enum:DeleteType)',
+    updated_at   timestamp    not null comment '사용자 및 관리자의 변경 시각',
+    created_at   timestamp    not null comment '사용자 및 관리자의 생성 시각',
     PRIMARY KEY (`id`),
     UNIQUE KEY `member_unique` (`user_id`)
 ) comment '사용자 및 관리자 테이블';
@@ -22,3 +22,16 @@ create table if not exists member_access_history
     failure_type                    varchar(32) comment '접속 실패 시 실패 분류 (Enum:AuthenticationFailureType)',
     access_at                       timestamp  not null comment '접속 시도 시각'
 ) comment '사용자 및 관리자 접속 기록 테이블';
+
+create table if not exists home
+(
+    home_id          bigint auto_increment primary key comment '주택 ID',
+    name             varchar(32)  not null comment '주택명',
+    manage_number    bigint       not null comment '주택관리번호',
+    apply_date_start date         not null comment '청약 접수 시작일',
+    apply_date_end   date         not null comment '청약 접수 종료일',
+    home_type        varchar(16)  not null comment '주택구분코드',
+    location_type    varchar(16)  not null comment '공급지역코드',
+    address          varchar(255) not null comment '공급위치',
+    url              varchar(255) not null comment '분양정보 URL'
+) comment '주택 청약 정보 테이블';
