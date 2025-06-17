3118b5a4e75a47bc445913767dea4c1a7ae5eb09
gkszm <gkszm636@gmail.com>
2025-06-17 20:27:37 +0900

    캘린더 화면 구성 기본 완료

--- src/main/java/me/synn3r/jipsa/core/mvc/calendar/mapper/CalendarMapper.java
+++ src/main/java/me/synn3r/jipsa/core/mvc/calendar/mapper/CalendarMapper.java
@@ -3,17 +3,23 @@ package me.synn3r.jipsa.core.mvc.calendar.mapper;
 import java.util.List;
 import me.synn3r.jipsa.core.batch.entity.House;
 import me.synn3r.jipsa.core.mvc.calendar.domain.CalendarResponse;
+import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
+import org.mapstruct.Named;
 
 @Mapper
 public interface CalendarMapper {
 
+  @IterableMapping(qualifiedByName = "convertToCalendarResponse")
   List<CalendarResponse> convertToCalendar(List<House> houseList);
 
+  @Named(value = "convertToCalendarResponse")
   @Mappings({
-    @Mapping(target = "")
+    @Mapping(source = "applyDateStart", target = "start"),
+    @Mapping(source = "applyDateEnd", target = "end"),
+    @Mapping(source = "name", target = "title"),
   })
   CalendarResponse convertToCalendarResponse(House house);
 
