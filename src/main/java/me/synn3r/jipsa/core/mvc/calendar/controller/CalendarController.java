596a57b5238c854fe0e2928f0117558ecd8b5b7b
윤정민 <gkszm63@hanssak.co.kr>
2025-07-09 19:25:42 +0900

    사용하지 않는 POST 맵핑 제거

--- src/main/java/me/synn3r/jipsa/core/mvc/calendar/controller/CalendarController.java
+++ src/main/java/me/synn3r/jipsa/core/mvc/calendar/controller/CalendarController.java
@@ -7,7 +7,6 @@ import me.synn3r.jipsa.core.mvc.calendar.service.CalendarService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,17 +15,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequiredArgsConstructor
 public class CalendarController {
 
-  private final CalendarService calendarService;
+    private final CalendarService calendarService;
 
-  @GetMapping
-  @PostMapping
-  public String calendar(
-    @RequestParam(name = "year", required = false) Integer year,
-    @RequestParam(name = "month", required = false) Integer month, Model model) {
-    List<CalendarResponse> calendarList = calendarService.getCalendar(year, month);
-    model.addAttribute("month", month);
-    model.addAttribute("year", year);
-    model.addAttribute("calendarList", calendarList);
-    return "pages/Calendar";
-  }
+    @GetMapping
+    public String calendar(
+      @RequestParam(name = "year", required = false) Integer year,
+      @RequestParam(name = "month", required = false) Integer month, Model model) {
+        List<CalendarResponse> calendarList = calendarService.getCalendar(year, month);
+        model.addAttribute("month", month);
+        model.addAttribute("year", year);
+        model.addAttribute("calendarList", calendarList);
+        return "pages/Calendar";
+    }
 }
