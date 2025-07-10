b93b299c619dea3da065f37537bd1fd5ce29bdf3
윤정민 <gkszm63@hanssak.co.kr>
2025-07-10 18:46:48 +0900

    인증 성공에 대한 로깅을 AuthenticationSuccessHandler 가 아닌 AuthenticationEventListener 로 전환

--- src/main/java/me/synn3r/jipsa/core/component/security/AuthenticationEventListener.java
+++ src/main/java/me/synn3r/jipsa/core/component/security/AuthenticationEventListener.java
@@ -3,9 +3,12 @@ package me.synn3r.jipsa.core.component.security;
 import lombok.RequiredArgsConstructor;
 import me.synn3r.jipsa.core.component.security.enumerations.AuthenticationFailureType;
 import me.synn3r.jipsa.core.component.security.logging.AuthenticationFailureLogger;
+import me.synn3r.jipsa.core.component.security.logging.AuthenticationSuccessLogger;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
 import org.springframework.security.authentication.event.AuthenticationFailureServiceExceptionEvent;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,7 +16,13 @@ import org.springframework.stereotype.Component;
 public class AuthenticationEventListener {
 
     private final AuthenticationFailureLogger authenticationFailureLogger;
+    private final AuthenticationSuccessLogger authenticationSuccessLogger;
 
+    @EventListener
+    public void authenticationSuccessEvent(AuthenticationSuccessEvent event) {
+        authenticationSuccessLogger.saveAuthenticationSuccessHistory(
+          (UserDetails) event.getAuthentication().getPrincipal());
+    }
 
     @EventListener
     public void authenticationFailureBadCredentialsEvent(AuthenticationFailureServiceExceptionEvent event) {
