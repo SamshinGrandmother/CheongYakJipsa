2ae7b94a2874787f7a77c5ef8d667aab5b04b559
윤정민 <gkszm63@hanssak.co.kr>
2025-10-28 17:41:04 +0900

    feat : 다국어 메시지 추가
    - 다국어 메시지 파일이 많아질 때를 대비해 카테고리 별로 파일 분리

--- src/main/java/me/synn3r/jipsa/core/config/WebConfiguration.java
+++ src/main/java/me/synn3r/jipsa/core/config/WebConfiguration.java
@@ -1,24 +1,28 @@
 package me.synn3r.jipsa.core.config;
 
-import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfiguration implements WebMvcConfigurer {
-  @Bean
-  public LayoutDialect layoutDialect() {
-    return new LayoutDialect();
-  }
+import lombok.extern.slf4j.Slf4j;
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 
-  @Bean
-  public MessageSource messageSource() {
-    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-    messageSource.setBasenames("messages");
-    messageSource.setDefaultEncoding("utf-8");
-    return messageSource;
-  }
+@Configuration
+@Slf4j
+public class WebConfiguration implements WebMvcConfigurer {
+
+	@Bean
+	public LayoutDialect layoutDialect() {
+		return new LayoutDialect();
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasenames("messages/base", "messages/validation");
+		messageSource.setDefaultEncoding("utf-8");
+		return messageSource;
+	}
 }
