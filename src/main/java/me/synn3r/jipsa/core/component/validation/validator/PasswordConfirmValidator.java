6e4a01044aa87982ec619bfc0f1320b575c2eacd
gkszm <gkszm636@gmail.com>
2025-10-28 22:16:49 +0900

    feat : 비밀번호 확인 오류 시 필드 변경
    - 비밀번호 확인 form 에서 오류 메시지 출력되도록 변경

--- src/main/java/me/synn3r/jipsa/core/component/validation/validator/PasswordConfirmValidator.java
+++ src/main/java/me/synn3r/jipsa/core/component/validation/validator/PasswordConfirmValidator.java
@@ -10,6 +10,16 @@ import me.synn3r.jipsa.core.component.validation.domain.ConfirmablePassword;
 public class PasswordConfirmValidator implements ConstraintValidator<PasswordConfirmValid, ConfirmablePassword> {
 	@Override
 	public boolean isValid(ConfirmablePassword value, ConstraintValidatorContext context) {
-		return Optional.ofNullable(value.getPassword()).orElse("").equals(value.getPasswordConfirm());
+		boolean isValid = Optional.ofNullable(value.getPassword()).orElse("").equals(value.getPasswordConfirm());
+
+		if (!isValid) {
+			context.disableDefaultConstraintViolation();
+
+			context.buildConstraintViolationWithTemplate(context.getDefaultConstraintMessageTemplate())
+				.addPropertyNode("passwordConfirm")
+				.addConstraintViolation();
+		}
+
+		return isValid;
 	}
 }
